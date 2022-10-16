b7faac1776b2dda1b987b6dcd0576bce9a933c94
naam studentc:/Users/bart-/OneDrive/Documenten/Repo's/FSWD-Projecten/venv_fswd/Scripts/python.exe <bart.roels@student.howest.be>
2022-10-16 12:49:11 +0200

    Cards + css grid system

--- script/app.js
+++ script/app.js
@@ -1,13 +1,36 @@
-function handleFloatingLabel() {
-   
+
+
+showResult = (data) => {
+    console.log(data);
+    // Get dom elements
+    const foto = document.querySelector(".js-foto1");
+    const name = document.querySelector(".js-naam1");
+    // Set photo url from API as src
+    foto.src = data.drinks[0].strDrinkThumb;
+    // Set name from API as text
+    name.innerHTML = data.drinks[0].strDrink;
 }
 
-function handlePasswordSwitcher() {
-    
-}
+getData = async (url) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+};
+
+let getAPI = async (lat, lon) => {
+  // build the url
+  url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+  // Get data from API
+  const data = await getData(url);
+  // If data is not empty
+  if (data.drinks) {
+    // Call show function
+    showResult(data);
+  }
+
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  // Get date from api
+  getAPI();
 });
